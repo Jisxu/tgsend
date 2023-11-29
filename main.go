e68955757037dd4fec8aa5ae6838f25520c484f5
jisxu <justinfox1206@gmail.com>
2023-11-29 16:17:52 +0800

    增加代理配置，允许指定配置文件路径

--- main.go
+++ main.go
@@ -4,42 +4,56 @@ import (
 	"flag"
 	"github.com/go-ini/ini"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"log"
 	"log/slog"
+	"net/http"
+	"net/url"
 	"os"
-	"path/filepath"
 	"strconv"
-	"strings"
 )
 
 func main() {
+	//read param
+	var text string
+	var configFile string
+	flag.StringVar(&text, "t", "hello", "要发送的信息，默认值为hello")
+	flag.StringVar(&configFile, "c", "config.ini", "配置文件路径")
+	flag.Parse()
 	// read config
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	cfg, err := ini.Load(configFile)
 	if err != nil {
-		panic(err)
+		slog.Error("failed:", err)
+		cfg := ini.Empty()
+		section := cfg.Section(ini.DefaultSection)
+		section.Key("apitoken").SetValue("")
+		section.Key("chatid").SetValue("")
+		section.Key("proxy").SetValue("socks5://127.0.0.1:1080")
+		err := cfg.SaveTo("config.ini")
+		if err != nil {
+			panic(err)
+		}
+		slog.Info("config not found,create at './config.ini'")
+		os.Exit(1)
 	}
-	dir = strings.ReplaceAll(dir, "\\", "/")
-	cfg, err := ini.Load(dir + "/config.ini")
-	if err != nil {
-		log.Fatal("Fail to read file: ", err)
-	}
-	slog.Info(dir)
 	section := cfg.Section(ini.DefaultSection)
 	apitoken := section.Key("apitoken")
 	chatid := section.Key("chatid")
-	//read param
-	var text string
-	flag.StringVar(&text, "t", "hello", "要发送的信息，默认值为hello")
-	flag.Parse()
+	proxy := section.Key("proxy")
 	// init bot
-	bot, err := tgbotapi.NewBotAPI(apitoken.String())
+	proxyUrl, err := url.Parse(proxy.String())
+	// add proxy
+	myClient := &http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(proxyUrl),
+		},
+	}
+	bot, err := tgbotapi.NewBotAPIWithClient(apitoken.String(), tgbotapi.APIEndpoint, myClient)
 	if err != nil {
 		panic(err)
 	}
 	bot.Debug = true
 	chatid_Int64, err := strconv.ParseInt(chatid.String(), 10, 64)
 	if err != nil {
-		slog.Error("convert faild,chatid is:" + chatid.String())
+		slog.Error("convert failed,chatId is:" + chatid.String())
 		panic(err)
 	}
 	msg := tgbotapi.NewMessage(chatid_Int64, text)
