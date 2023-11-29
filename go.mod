159fbe2535ea60a774d68c0c3c1dffc3a7e2703c
jisxu <justinfox1206@gmail.com>
2023-11-29 14:02:50 +0800

    finished

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module tgsend
 go 1.21
 
 require (
-	github.com/go-ini/ini v1.67.0 // indirect
-	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
+	github.com/go-ini/ini v1.67.0
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
 )
+
+require github.com/stretchr/testify v1.8.4 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"github.com/go-ini/ini"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"log"
+	"log/slog"
+	"strconv"
 )
 
 func main() {
@@ -11,8 +14,28 @@ func main() {
 	if err != nil {
 		log.Fatal("Fail to read file: ", err)
 	}
-	apitoken := cfg.Section(ini.DefaultSection).Key("apitoken")
-	chatid := cfg.Section(ini.DefaultSection).Key("chatid")
-	fmt.Println(apitoken.String())
-	fmt.Println(chatid.String())
+	// read config
+	section := cfg.Section(ini.DefaultSection)
+	apitoken := section.Key("apitoken")
+	chatid := section.Key("chatid")
+	//read param
+	var text string
+	flag.StringVar(&text, "t", "hello", "要发送的信息，默认值为hello")
+	flag.Parse()
+	// init bot
+	bot, err := tgbotapi.NewBotAPI(apitoken.String())
+	if err != nil {
+		panic(err)
+	}
+	bot.Debug = true
+	chatid_Int64, err := strconv.ParseInt(chatid.String(), 10, 64)
+	if err != nil {
+		slog.Error("convert faild,chatid is:" + chatid.String())
+		panic(err)
+	}
+	msg := tgbotapi.NewMessage(chatid_Int64, text)
+	_, err = bot.Send(msg)
+	if err != nil {
+		panic(err)
+	}
 }
