55f7b5f3a3572c551ecaa0af74d464c19cfe9a35
jisxu <justinfox1206@gmail.com>
2023-11-30 16:36:08 +0800

    使用chatgpt优化代码和readme

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# tgsend
-通过tg机器人发送消息
+# tgSend
+
+`tgSend` 是一个用于通过 Telegram 发送消息的简单命令行工具。
+
+## 功能
+
+- 通过命令行参数或配置文件发送指定消息到 Telegram 聊天。
+- 支持配置文件，方便管理 API 令牌、聊天 ID 等信息。
+- 支持代理设置，可通过配置文件指定代理地址。
+
+## 安装
+
+```bash
+go get -u github.com/Jisxu/tgsend
+```
+
+## 用法
+
+```bash
+tgsend -t "你要发送的消息"
+```
+
+可用的命令行参数：
+
+- `-t`: 要发送的消息内容，默认为 "hello"。
+- `-c`: 配置文件路径，默认为 "config.ini"。
+
+## 配置文件
+
+`config.ini` 文件包含以下配置项：
+
+```ini
+[default]
+apitoken = YOUR_TELEGRAM_API_TOKEN
+chatid = YOUR_TELEGRAM_CHAT_ID
+proxy = socks5://127.0.0.1:1080
+```
+
+- `apitoken`: Telegram 机器人的 API 令牌。
+- `chatid`: 目标聊天的 ID。
+- `proxy`: 代理地址，可选配置。默认为 socks5://127.0.0.1:1080。
+
+## 示例
+
+```bash
+# 发送消息到默认聊天
+tgsend -t "你好，Telegram！"
+
+# 使用自定义配置文件路径
+tgsend -t "自定义消息" -c path/to/custom-config.ini
+```
+
+## 注意事项
+
+- 请确保 Telegram API 令牌和聊天 ID 的正确性。
+- 如果配置文件不存在，会在程序运行时自动生成默认配置文件。
+
+## 许可证
+
+该项目采用 [MIT 许可证](LICENSE)。
+
+请替换文档中的 `YOUR_TELEGRAM_API_TOKEN` 和 `YOUR_TELEGRAM_CHAT_ID` 为你的 Telegram 机器人的 API 令牌和目标聊天的 ID。
--- main.go
+++ main.go
@@ -2,63 +2,113 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/go-ini/ini"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"log/slog"
+	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"strconv"
 )
 
+const (
+	defaultText       = "hello"
+	defaultConfigFile = "config.ini"
+	defaultProxy      = "socks5://127.0.0.1:1080"
+	configKeyApiToken = "apitoken"
+	configKeyChatID   = "chatid"
+	configKeyProxy    = "proxy"
+)
+
+var (
+	configSection = ini.DefaultSection
+)
+
 func main() {
-	//read param
-	var text string
-	var configFile string
-	flag.StringVar(&text, "t", "hello", "要发送的信息，默认值为hello")
-	flag.StringVar(&configFile, "c", "config.ini", "配置文件路径")
+	// 读取参数
+	var text, configFile string
+	flag.StringVar(&text, "t", defaultText, "要发送的信息，默认值为hello")
+	flag.StringVar(&configFile, "c", defaultConfigFile, "配置文件路径")
 	flag.Parse()
-	// read config
-	cfg, err := ini.Load(configFile)
+
+	// 读取配置
+	cfg, err := loadConfig(configFile)
 	if err != nil {
-		slog.Error("failed:", err)
-		cfg := ini.Empty()
-		section := cfg.Section(ini.DefaultSection)
-		section.Key("apitoken").SetValue("")
-		section.Key("chatid").SetValue("")
-		section.Key("proxy").SetValue("socks5://127.0.0.1:1080")
-		err := cfg.SaveTo("config.ini")
-		if err != nil {
-			panic(err)
-		}
-		slog.Info("config not found,create at './config.ini'")
-		os.Exit(1)
+		log.Fatalf("无法加载配置: %v", err)
 	}
-	section := cfg.Section(ini.DefaultSection)
-	apitoken := section.Key("apitoken")
-	chatid := section.Key("chatid")
-	proxy := section.Key("proxy")
-	// init bot
-	proxyUrl, err := url.Parse(proxy.String())
-	// add proxy
-	myClient := &http.Client{
-		Transport: &http.Transport{
-			Proxy: http.ProxyURL(proxyUrl),
-		},
-	}
-	bot, err := tgbotapi.NewBotAPIWithClient(apitoken.String(), tgbotapi.APIEndpoint, myClient)
+
+	// 获取配置值
+	apitoken := getConfigValue(cfg, configKeyApiToken, "")
+	chatid := getConfigValue(cfg, configKeyChatID, "")
+	proxy := getConfigValue(cfg, configKeyProxy, defaultProxy)
+
+	// 初始化机器人
+	proxyURL, err := url.Parse(proxy)
 	if err != nil {
-		panic(err)
+		log.Fatalf("代理URL解析失败: %v", err)
 	}
-	bot.Debug = true
-	chatid_Int64, err := strconv.ParseInt(chatid.String(), 10, 64)
+
+	bot, err := initializeBot(apitoken, proxyURL)
 	if err != nil {
-		slog.Error("convert failed,chatId is:" + chatid.String())
-		panic(err)
+		log.Fatalf("机器人初始化失败: %v", err)
 	}
-	msg := tgbotapi.NewMessage(chatid_Int64, text)
+
+	// 转换聊天ID为int64
+	chatID, err := strconv.ParseInt(chatid, 10, 64)
+	if err != nil {
+		log.Fatalf("转换失败，chatId 为: %s", chatid)
+	}
+
+	// 发送消息
+	msg := tgbotapi.NewMessage(chatID, text)
 	_, err = bot.Send(msg)
 	if err != nil {
-		panic(err)
+		log.Fatalf("消息发送失败: %v", err)
 	}
+}
+
+func loadConfig(configFile string) (*ini.File, error) {
+	cfg, err := ini.Load(configFile)
+	if err != nil {
+		log.Printf("加载配置失败，创建默认配置: %v", err)
+		cfg = ini.Empty()
+		section := cfg.Section(configSection)
+		section.Key(configKeyApiToken).SetValue("")
+		section.Key(configKeyChatID).SetValue("")
+		section.Key(configKeyProxy).SetValue(defaultProxy)
+
+		if err := cfg.SaveTo(defaultConfigFile); err != nil {
+			log.Fatalf("保存默认配置失败: %v", err)
+		}
+
+		fmt.Println("未找到配置文件，在当前目录创建新的配置文件 'config.ini'")
+		os.Exit(1)
+	}
+	return cfg, nil
+}
+
+func getConfigValue(cfg *ini.File, key, defaultValue string) string {
+	section := cfg.Section(configSection)
+	value := section.Key(key).String()
+	if value == "" {
+		return defaultValue
+	}
+	return value
+}
+
+func initializeBot(apiToken string, proxyURL *url.URL) (*tgbotapi.BotAPI, error) {
+	client := &http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(proxyURL),
+		},
+	}
+
+	bot, err := tgbotapi.NewBotAPIWithClient(apiToken, tgbotapi.APIEndpoint, client)
+	if err != nil {
+		return nil, err
+	}
+
+	bot.Debug = true
+	return bot, nil
 }
